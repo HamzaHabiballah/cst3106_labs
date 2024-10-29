5c105788b9f7502ab099eeb8c9f0ca854b7c3b22
HamzaHabiballah <hamzahabiballah1738@gmail.com>
2024-10-28 21:44:10 -0400

    just added Lab06

---
+++ Lab6/Dice.js
@@ -0,0 +1,27 @@
+class Dice {
+    constructor(numDice = 5) {
+      this.numDice = numDice;
+      this.min = 1; 
+      this.max = 6; 
+      this.history = []; 
+      this.currentValues = Array(numDice).fill(0); 
+    }
+  
+    
+    roll() {
+      this.currentValues = this.currentValues.map(() => {
+        const nextValue = Math.floor(this.min + Math.random() * this.max);
+        this.history.push(nextValue); 
+        return nextValue; 
+      });
+      return this.currentValues; 
+    }
+  
+    
+    showHistory() {
+      return this.history;
+    }
+  }
+  
+  module.exports = Dice; 
+  
